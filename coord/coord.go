15e6f36a201b093c0fe968efa0f2879d9ed9a6e5
nico <brun.nicolas21@gmail.com>
2018-11-12 14:25:22 +0100

    gofmt

--- coord/coord.go
+++ coord/coord.go
@@ -1,65 +1,65 @@
 package coord
 
 import (
-  "math"
+	"math"
 )
 
 const Tolerance = 0.00000001
 
 const (
-  rad2deg = 180 / math.Pi
-  deg2rad = math.Pi / 180
+	rad2deg = 180 / math.Pi
+	deg2rad = math.Pi / 180
 )
 
 const (
-  eex         = 0.006694385 //earth excentricity squared (see vallado for reference)
-  flattening   = 0.003352813178
-  earthRadius = 6378.1363
+	eex         = 0.006694385 //earth excentricity squared (see vallado for reference)
+	flattening  = 0.003352813178
+	earthRadius = 6378.1363
 )
 
 func GeodeticFromECEF(x, y, z float64) (float64, float64, float64) {
-  lat, lon, alt := ecef2Geodetic(x, y, z)
-  return lat*rad2deg, lon * rad2deg, alt
+	lat, lon, alt := ecef2Geodetic(x, y, z)
+	return lat * rad2deg, lon * rad2deg, alt
 }
 
 func GeodeticToECEF(lat, lon, alt float64) (float64, float64, float64) {
-  alt *= 1000
-  lat *= deg2rad
-  lon *= deg2rad
+	alt *= 1000
+	lat *= deg2rad
+	lon *= deg2rad
 
-  sin := math.Sin(lat) * math.Sin(lat)
-	n := (earthRadius*1000) * math.Pow(1-flattening*(2-flattening)*sin, -0.5)
+	sin := math.Sin(lat) * math.Sin(lat)
+	n := (earthRadius * 1000) * math.Pow(1-flattening*(2-flattening)*sin, -0.5)
 
-  x := (n + alt) * math.Cos(lat) * math.Cos(lon)
+	x := (n + alt) * math.Cos(lat) * math.Cos(lon)
 	y := (n + alt) * math.Cos(lat) * math.Sin(lon)
 	z := (n*(1-eex) + alt) * math.Sin(lat)
 
-  return x/1000, y/1000, z/1000
+	return x / 1000, y / 1000, z / 1000
 }
 
 func GeocentricFromECEF(x, y, z float64) (float64, float64, float64) {
-  lat, lon, alt := ecef2Geodetic(x, y, z)
-  return math.Atan((1-eex)*math.Tan(lat))*rad2deg, lon * rad2deg, alt
+	lat, lon, alt := ecef2Geodetic(x, y, z)
+	return math.Atan((1-eex)*math.Tan(lat)) * rad2deg, lon * rad2deg, alt
 }
 
 func ecef2Geodetic(x, y, z float64) (float64, float64, float64) {
-  norm := math.Sqrt(x * x + y * y + z * z)
-	radius := math.Sqrt(x * x + y * y)
+	norm := math.Sqrt(x*x + y*y + z*z)
+	radius := math.Sqrt(x*x + y*y)
 
 	lon := math.Atan2(y/earthRadius, x/earthRadius)
-	delta := math.Asin(z/norm)
+	delta := math.Asin(z / norm)
 
 	lat := delta
-  var hell float64
-  for {
+	var hell float64
+	for {
 		delta = lat
 		sin := math.Sin(lat)
-		c := earthRadius / math.Sqrt(1 - (eex * sin * sin))
-		lat = math.Atan((z + c * eex * sin) / radius)
+		c := earthRadius / math.Sqrt(1-(eex*sin*sin))
+		lat = math.Atan((z + c*eex*sin) / radius)
 		if diff := math.Abs(delta - lat); diff <= Tolerance {
-      hell = (radius /math.Cos(lat)) - c
+			hell = (radius / math.Cos(lat)) - c
 			break
 		}
 	}
-  return lat, lon, hell
+	return lat, lon, hell
 }
--- coord/coord_test.go
+++ coord/coord_test.go
@@ -1,29 +1,29 @@
 package coord
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func ExampleGeodeticFromECEF() {
-  x, y, z := 6524.834, 6862.875, 6448.296
-  lat, lon, alt := GeodeticFromECEF(x, y, z)
-  fmt.Printf("lat: %8.6f°, lon: %7.4f°, alt: %6.2f km", lat, lon, alt)
-  // Output:
-  // lat: 34.352496°, lon: 46.4464°, alt: 5085.22 km
+	x, y, z := 6524.834, 6862.875, 6448.296
+	lat, lon, alt := GeodeticFromECEF(x, y, z)
+	fmt.Printf("lat: %8.6f°, lon: %7.4f°, alt: %6.2f km", lat, lon, alt)
+	// Output:
+	// lat: 34.352496°, lon: 46.4464°, alt: 5085.22 km
 }
 
 func ExampleGeodeticToECEF() {
-  lat, lon, alt := 34.352495, 46.4464, 5085.22
-  x, y, z := GeodeticToECEF(lat, lon, alt)
-  fmt.Printf("x: %.3f, y: %.3f, z: %.3f", x, y, z)
-  // Output:
-  // x: 6524.834, y: 6862.875, z: 6448.296
+	lat, lon, alt := 34.352495, 46.4464, 5085.22
+	x, y, z := GeodeticToECEF(lat, lon, alt)
+	fmt.Printf("x: %.3f, y: %.3f, z: %.3f", x, y, z)
+	// Output:
+	// x: 6524.834, y: 6862.875, z: 6448.296
 }
 
 func ExampleGeocentricFromECEF() {
-  x, y, z := 6524.834, 6862.875, 6448.296
-  lat, lon, _ := GeocentricFromECEF(x, y, z)
-  fmt.Printf("lat: %8.6f°, lon: %7.4f°", lat, lon)
-  // Output:
-  // lat: 34.173429°, lon: 46.4464°
+	x, y, z := 6524.834, 6862.875, 6448.296
+	lat, lon, _ := GeocentricFromECEF(x, y, z)
+	fmt.Printf("lat: %8.6f°, lon: %7.4f°", lat, lon)
+	// Output:
+	// lat: 34.173429°, lon: 46.4464°
 }
