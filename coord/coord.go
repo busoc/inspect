2bc37d7298b80f15f18d00649cb7eb310ea22d20
nico <brun.nicolas21@gmail.com>
2018-11-12 14:25:04 +0100

    coord package + gofmt

--- coord/coord.go
+++ coord/coord.go
@@ -6,16 +6,35 @@ import (
 
 const Tolerance = 0.00000001
 
-const rad2deg = 180 / math.Pi
+const (
+  rad2deg = 180 / math.Pi
+  deg2rad = math.Pi / 180
+)
 
 const (
   eex         = 0.006694385 //earth excentricity squared (see vallado for reference)
+  flattening   = 0.003352813178
   earthRadius = 6378.1363
 )
 
 func GeodeticFromECEF(x, y, z float64) (float64, float64, float64) {
   lat, lon, alt := ecef2Geodetic(x, y, z)
   return lat*rad2deg, lon * rad2deg, alt
+}
+
+func GeodeticToECEF(lat, lon, alt float64) (float64, float64, float64) {
+  alt *= 1000
+  lat *= deg2rad
+  lon *= deg2rad
+
+  sin := math.Sin(lat) * math.Sin(lat)
+	n := (earthRadius*1000) * math.Pow(1-flattening*(2-flattening)*sin, -0.5)
+
+  x := (n + alt) * math.Cos(lat) * math.Cos(lon)
+	y := (n + alt) * math.Cos(lat) * math.Sin(lon)
+	z := (n*(1-eex) + alt) * math.Sin(lat)
+
+  return x/1000, y/1000, z/1000
 }
 
 func GeocentricFromECEF(x, y, z float64) (float64, float64, float64) {
--- coord/coord_test.go
+++ coord/coord_test.go
@@ -12,6 +12,14 @@ func ExampleGeodeticFromECEF() {
   // lat: 34.352496°, lon: 46.4464°, alt: 5085.22 km
 }
 
+func ExampleGeodeticToECEF() {
+  lat, lon, alt := 34.352495, 46.4464, 5085.22
+  x, y, z := GeodeticToECEF(lat, lon, alt)
+  fmt.Printf("x: %.3f, y: %.3f, z: %.3f", x, y, z)
+  // Output:
+  // x: 6524.834, y: 6862.875, z: 6448.296
+}
+
 func ExampleGeocentricFromECEF() {
   x, y, z := 6524.834, 6862.875, 6448.296
   lat, lon, _ := GeocentricFromECEF(x, y, z)
