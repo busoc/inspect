2e457c8d36058593d1c1fb586a915adb6b48659a
nico <brun.nicolas21@gmail.com>
2018-11-13 10:19:32 +0100

    move reading/fetching tle in its own func

--- cmd/inspect/inspect.go
+++ cmd/inspect/inspect.go
@@ -50,6 +50,7 @@ func main() {
 		East:  SAALonMax,
 		West:  SAALonMin,
 	}
+
 	var p printer
 	flag.Var(&saa, "r", "saa area")
 	copydir := flag.String("t", os.TempDir(), "temp dir")
@@ -63,42 +64,10 @@ func main() {
 	flag.BoolVar(&p.DMS, "dms", false, "dms")
 	flag.Parse()
 
-	var t celest.Trajectory
-	digest := md5.New()
-	if resp, err := http.Get(flag.Arg(0)); err == nil {
-		if resp.StatusCode != http.StatusOK {
-			log.Fatalf("fail to fetch data from %s (%s)", flag.Arg(0), resp.Status)
-		}
-		var w io.Writer = digest
-		suffix := "-" + time.Now().Format("20060102_150405")
-		if err := os.MkdirAll(*copydir, 0755); err != nil && !os.IsExist(err) {
-			log.Fatalln(err)
-		}
-		if f, err := os.Create(path.Join(*copydir, path.Base(flag.Arg(0)+suffix))); err == nil {
-			defer f.Close()
-			w = io.MultiWriter(f, w)
-		}
-		if err := t.Scan(io.TeeReader(resp.Body, w), *sid); err != nil {
-			log.Fatalln(err)
-		}
-		log.Printf("parsing TLE from %s done (md5: %x, last-modified: %s)", flag.Arg(0), digest.Sum(nil), resp.Header.Get("last-modified"))
-		resp.Body.Close()
-	} else {
-		for _, p := range flag.Args() {
-			r, err := os.Open(p)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			if err := t.Scan(io.TeeReader(r, digest), *sid); err != nil {
-				log.Fatalln(err)
-			}
-			s, _ := r.Stat()
-			log.Printf("parsing TLE from %s done (md5: %x, last-modified: %s)", p, digest.Sum(nil), s.ModTime().Format(time.RFC1123))
-			r.Close()
-			digest.Reset()
-		}
+	t, err := fetchTLE(flag.Args(), *copydir, *sid)
+	if err != nil {
+		log.Fatalln(err)
 	}
-
 	rs, err := t.Predict(*period, *interval, &saa)
 	if err != nil {
 		log.Fatalln(err)
@@ -114,4 +83,43 @@ func main() {
 	if err := p.Print(w, rs); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func fetchTLE(ps []string, copydir string, sid int) (*celest.Trajectory, error) {
+	var t celest.Trajectory
+	digest := md5.New()
+	if resp, err := http.Get(ps[0]); err == nil {
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("fail to fetch data from %s (%s)", ps[0], resp.Status)
+		}
+		var w io.Writer = digest
+		suffix := "-" + time.Now().Format("20060102_150405")
+		if err := os.MkdirAll(copydir, 0755); err != nil && !os.IsExist(err) {
+			return nil, err
+		}
+		if f, err := os.Create(path.Join(copydir, path.Base(ps[0]+suffix))); err == nil {
+			defer f.Close()
+			w = io.MultiWriter(f, w)
+		}
+		if err := t.Scan(io.TeeReader(resp.Body, w), sid); err != nil {
+			return nil, err
+		}
+		log.Printf("parsing TLE from %s done (md5: %x, last-modified: %s)", ps[0], digest.Sum(nil), resp.Header.Get("last-modified"))
+		resp.Body.Close()
+	} else {
+		for _, p := range ps {
+			r, err := os.Open(p)
+			if err != nil {
+				return nil, err
+			}
+			if err := t.Scan(io.TeeReader(r, digest), sid); err != nil {
+				return nil, err
+			}
+			s, _ := r.Stat()
+			log.Printf("parsing TLE from %s done (md5: %x, last-modified: %s)", p, digest.Sum(nil), s.ModTime().Format(time.RFC1123))
+			r.Close()
+			digest.Reset()
+		}
+	}
+	return &t, nil
 }
